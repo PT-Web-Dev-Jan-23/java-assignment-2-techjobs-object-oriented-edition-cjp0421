e672b169b01dd351e525a197749fd222d3653096
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-05-03 20:32:34 -0500

    completed task 4 - all task 4 tests pass

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,7 +1,12 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
@@ -9,4 +14,34 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class JobTest {
 
+    @Test
+    public void testSettingJobId(){
+        Job job1 = new Job();
+        Job job2 = new Job();
+        assertNotEquals(job1,job2);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertEquals(job1.getName(),"Product tester");
+        assertEquals(job1.getEmployer().getValue(), "ACME");
+        assertEquals(job1.getLocation().getValue(), "Desert");
+        assertEquals(job1.getPositionType().getValue(), "Quality control");
+        assertEquals(job1.getCoreCompetency().getValue(), "Persistence");
+
+        assertTrue(job1 instanceof Job);
+        assertTrue(job1.getEmployer() instanceof Employer);
+        assertTrue(job1.getLocation() instanceof Location);
+        assertTrue(job1.getPositionType() instanceof PositionType);
+        assertTrue(job1.getCoreCompetency() instanceof CoreCompetency);
+    }
+
+    @Test
+    public void testJobsForEquality(){
+        Job job3 = new Job("Big Cheese", new Employer("MoneyMaker, Inc."), new Location("Big City"), new PositionType("Decision maker"), new CoreCompetency("Shifting Paradigms"));
+        Job job4 = new Job("Big Cheese", new Employer("MoneyMaker, Inc."), new Location("Big City"), new PositionType("Decision maker"), new CoreCompetency("Shifting Paradigms"));
+
+        assertFalse(job3.equals(job4));
+    }
 }
